b1d1beeddefc38f4cd205efbc45c09a43715feb3
awesomething <79168736+PairApp@users.noreply.github.com>
2023-05-10 13:06:36 -0400

    first commit

---
+++ routes/openai.js
@@ -0,0 +1,130 @@
+import express from 'express'
+import axios from 'axios'
+import dotenv from 'dotenv'
+import { openai } from '../index.js'
+
+dotenv.config()
+const router = express.Router()
+
+router.post('/text', async (req, res) =>{
+    try {
+        const {text, activeChatId} = req.body;
+        // console.log("text:", text)
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",                
+            prompt: text,
+            temperature: 0.5,
+            max_tokens: 2048,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0
+        })
+
+        await axios.post(
+            `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+            { text: response.data.choices[0].text },
+            {
+              headers: {
+                "Project-ID": process.env.PROJECT_ID,
+                "User-Name": process.env.BOT_USER_NAME,
+                "User-Secret": process.env.BOT_USER_SECRET,
+              },
+            }
+          );
+        res.status(200).json({text: response.data.choices[0].text})
+    } catch (error) {
+        console.log('error', error)
+        res.status(500).json({ error: error.message })
+    }
+})
+
+router.post('/code', async (req, res) =>{
+    try {
+        const {text, activeChatId} = req.body;
+        // console.log("text:", text)
+        const response = await openai.createCompletion({
+            model: "code-davinci-002",
+            prompt: text,
+            temperature: 0.5,
+            max_tokens: 2048,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0
+        })
+
+        await axios.post(
+            `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+            { text: response.data.choices[0].text },
+            {
+              headers: {
+                "Project-ID": process.env.PROJECT_ID,
+                "User-Name": process.env.BOT_USER_NAME,
+                "User-Secret": process.env.BOT_USER_SECRET,
+              },
+            }
+          );
+        res.status(200).json({text: response.data.choices[0].text})
+    } catch (error) {
+        console.log('error', error)
+        res.status(500).json({ error: error.message })
+    }
+})
+
+router.post('/assist', async (req, res) =>{
+    try {
+        const {text} = req.body;
+        
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `Finish my thought: ${text}`,
+            temperature: 0.5,
+            max_tokens: 1024,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0
+        })
+        res.status(200).json({text: response.data.choices[0].text})
+    } catch (error) {
+        console.log('error', error)
+        res.status(500).json({ error: error.message })
+    }
+})
+
+router.post("/text", async (req, res) => {
+try {
+    const { text, activeChatId } = req.body;
+
+    const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+    { role: "system", content: "You are a helpful assistant." }, // this represents the bot and what role they will assume
+    //{ role: "user", content: text }, // the message that the user sends
+
+    // BONUS NOTE: you can also provide a list of messages to the bot to give context
+    // and the bot can use that information to respond to the user as needed, ie adding:
+    { role: "assistant", content: "The weather sucks today." },
+    //to the above messages array, and then asking it this question:
+    `how is the weather today?`
+],
+     });
+
+   await axios.post(
+       `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+         { text: response.data.choices[0].message.content },
+       {
+         headers: {
+           "Project-ID": process.env.PROJECT_ID,           
+           "User-Name": process.env.BOT_USER_NAME,
+           "User-Secret": process.env.BOT_USER_SECRET,
+         },
+       }
+    );
+
+     res.status(200).json({ text: response.data.choices[0].message.content });  } 
+     catch (error) {
+     console.error("error", error.response.data.error);
+     res.status(500).json({ error: error.message });
+   }
+ });
+
+export default router
